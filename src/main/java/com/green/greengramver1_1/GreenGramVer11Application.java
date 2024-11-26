be321e603365b0b4a14d5b984cd9317ee140a4bd
boyunsim <tlaqhdbs12@gmail.com>
2024-11-26 12:43:36 +0900

    2024-11-26(first)

---
+++ src/main/java/com/green/greengramver1_1/GreenGramVer11Application.java
@@ -0,0 +1,13 @@
+package com.green.greengramver1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenGramVer11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenGramVer11Application.class, args);
+	}
+
+}
